c86f387ca4de1d481909f990033bd74e80e2a688
Stijn <mail@stijnh.nl>
2013-08-31 22:33:04 +0200

    Added graphs to webpage

--- www/index.php
+++ www/index.php
@@ -4,11 +4,9 @@ define('MAX_TIMEOUT', 60*60);
 
 $data = @json_decode(@file_get_contents('data.txt'));
 $ip = null;
-$last_contact = 0;
 
 if ($data !== null) {
     $ip = $data->ip;
-    $prev_probe = $data->time;
 }
 ?>
 <!DOCTYPE html>
@@ -31,49 +29,59 @@ if ($data !== null) {
             </div>
         </div>
         <ul class="tabs">
-            <li class="active"><i class="icon-home"></i> Overview</li>
-            <li><i class="icon-dashboard"></i> CPU</li>
-            <li><i class="icon-tasks"></i> Services</li>
+            <li class="active" id="tab-overview"><i class="icon-home"></i> Overview</li>
+            <li id="tab-graphs"><i class="icon-dashboard"></i> Graphs</li>
+            <li id="tab-services"><i class="icon-tasks"></i> Services</li>
         </ul>
         <div class="content">
-            <div class="row">
-                <i class="icon-time"></i> Uptime <div id="status-uptime" class="right"></div>
+          <div id="page-overview">
+              <div class="row">
+                  <i class="icon-time"></i> Uptime <div id="status-uptime" class="right"></div>
+              </div>
+              <div class="row">
+                  <i class="icon-info-sign"></i> IP address <div id="status-ip" class="right"></div>
+              </div>
+              <div class="row">
+                  <i class="icon-fire"></i> Temperature <div id="status-temperature" class="right"></div>
+              </div>
+              <div class="row">
+                  <i class="icon-dashboard"></i> CPU usage <div id="status-cpu" class="right">20%</div>
+              </div>
+              <div class="row">
+                  <i class="icon-th-list"></i> Memory
+                      <div class="right">
+                          <span id="status-memory-used"></span> / <span id="status-memory-total"></span>
+                      </div>
+              </div>
+              <div class="row">
+                  <i class="icon-hdd"></i> Storage
+                  <div class="right">
+                      <span id="status-storage-used"></span> / <span id="status-storage-total"></span>
+                  </div>
+              </div>
+              <div class="row">
+                  <i class="icon-refresh"></i> Network
+                  <div class="right">
+                      <i class="icon-arrow-up"></i> <span id="status-network-sent"></span> / <i class="icon-arrow-down"></i> <span id="status-network-recv"></span>
+                  </div>
+              </div>
             </div>
-            <div class="row">
-                <i class="icon-info-sign"></i> IP address <div id="status-ip" class="right"></div>
+            <div id="page-graphs" style="display: none;">
+                <canvas id="graph-cpu" width="600" height="200"></canvas>
+                <canvas id="graph-memory" width="600" height="200"></canvas>
             </div>
-            <div class="row">
-                <i class="icon-fire"></i> Temperature <div id="status-temperature" class="right"></div>
-            </div>
-            <div class="row">
-                <i class="icon-dashboard"></i> CPU usage <div id="status-cpu" class="right">20%</div>
-            </div>
-            <div class="row">
-                <i class="icon-th-list"></i> Memory
-                    <div class="right">
-                        <span id="status-memory-used"></span> / <span id="status-memory-total"></span>
-                    </div>
-            </div>
-            <div class="row">
-                <i class="icon-hdd"></i> Storage
-                <div class="right">
-                    <span id="status-storage-used"></span> / <span id="status-storage-total"></span>
-                </div>
-            </div>
-            <div class="row">
-                <i class="icon-refresh"></i> Network
-                <div class="right">
-                    <i class="icon-arrow-up"></i> <span id="status-network-send"></span> / <i class="icon-arrow-down"></i> <span id="status-network-recv"></span>
-                </div>
+            <div id="page-services" style="display: none;">
+              <div id="services"></div>
             </div>
         </div>
 
         <script src="js/jquery-2.0.3.min.js" text="text/javascript"></script>
-        <script src="js/measure.js" type="text/javascript"></script>
+        <script src="js/main.js" type="text/javascript"></script>
 <?php if ($ip) { ?>
         <script type="text/javascript">
             $(function() {
-                openConnection("ws://<?php echo $ip; ?>:8080");
+                initTabs();
+                connect("ws://<?php echo $ip; ?>:8080");
             });
         </script>
 <?php } ?>
---
+++ www/js/main.js
@@ -0,0 +1,186 @@
+var initTabs = function() {
+    var activeTab = 'overview';
+
+    $('[id^=tab-]').on('click', function() {
+        $('#tab-' + activeTab).removeClass('active')
+        $('#page-' + activeTab).hide();
+
+        activeTab = $(this).attr('id').substr('tab-'.length)
+
+        $('#tab-' + activeTab).addClass('active')
+        $('#page-' + activeTab).show();
+    });
+};
+
+var formatTimeDiff = function(diff) {
+    diff = Math.floor(diff);
+
+    var sec = diff % 60;
+    var out = sec + ' second' + (sec == 1 ? '' : 's');
+
+    diff = Math.floor(diff / 60);
+    if (diff <= 0) return out;
+
+    var min = diff % 60;
+    out = min + ' minute' + (min == 1 ? '' : 's') + ', ' + out;
+
+    diff = Math.floor(diff / 60);
+    if (diff <= 0) return out;
+
+    var hour = diff % 24;
+    out = hour + ' hour' + (hour == 1 ? '' : 's') + ', ' + out;
+
+    diff = Math.floor(diff / 24);
+    if (diff <= 0) return out;
+
+    var day = diff;
+    out = day + ' day' + (day == 1 ? '' : 's') + ', ' + out;
+
+    return out;
+};
+
+var formatBytes = function(n, dec) {
+    if (n < 1000) return n.toFixed(dec) + 'B';
+
+    n /= 1000;
+    if (n < 1000) return n.toFixed(dec) + 'kB'
+
+    n /= 1000;
+    if (n < 1000) return n.toFixed(dec) + 'MB'
+
+    n /= 1000;
+    return n.toFixed(dec) + 'GB'
+};
+
+var drawLineChart = function(canvas, data, color, bounds) {
+    var ctx = $(canvas)[0].getContext('2d');
+    var w = $(canvas)[0].width;
+    var h = $(canvas)[0].height;
+
+    ctx.clearRect(0, 0, w, h);
+
+    ctx.fillStyle = 'black';
+    ctx.strokeStyle = 'red';
+    ctx.lineJoin = 'round';
+    ctx.lineWidth = 5;
+    ctx.beginPath();
+
+    $.each(data, function(i, point) {
+        var x = w * (point[0] - bounds[0][0]) / (bounds[0][1] - bounds[0][0]);
+        var y = h * (point[1] - bounds[1][0]) / (bounds[1][1] - bounds[1][0]);
+
+        if (i == 0) {
+            ctx.moveTo(x, y);
+        } else {
+            ctx.lineTo(x, y);
+        }
+    });
+
+    ctx.stroke();
+
+
+    var t = 2;
+    ctx.fillStyle = 'hsl(0, 0%, 80%)';
+    ctx.fillRect(0, 0, t, h);
+    ctx.fillRect(0, 0, w, t);
+    ctx.fillRect(w - t, 0, t, h);
+    ctx.fillRect(0, h - t, w, t);
+};
+
+var connect = function(addr) {
+    $('[id^=status-]').html('Connecting...');
+    $('#status').removeClass('offline').html('Connecting...');
+
+    var cpuSeries = [];
+    var memorySeries = [];
+
+    var callbacks = {
+        kernel: function(data) {
+            $('#status-kernel').html(data);
+        },
+        uptime: function(data) {
+            $('#status-uptime').html(formatTimeDiff(data));
+        },
+        temperature: function(data) {
+            $('#status-temperature').html((data / 1000).toFixed(2)
+                    + '&deg;C');
+        },
+        memory: function(data) {
+            $('#status-memory-used').html(formatBytes(data.used, 2));
+            $('#status-memory-total').html(formatBytes(data.total, 2));
+            $('#status-memory-percent').html(formatBytes(
+                    data.used / data.total, 2));
+
+            var now = new Date().getTime();
+            memorySeries.push(data, [now, data.used / data.total * 100]);
+
+            drawLineChart('#graph-memory', memorySeries, 'red', [[now - 100000, now], [100, 0]]);
+        },
+        storage: function(data) {
+            $('#status-storage-used').html(formatBytes(data.used, 2));
+            $('#status-storage-total').html(formatBytes(data.total, 2));
+            $('#status-storage-percent').html(formatBytes(
+                    data.used / data.total, 2));
+        },
+        cpu: function(data) {
+            $('#status-cpu').html(data.toFixed(2) + '%');
+
+            var now = new Date().getTime();
+            cpuSeries.push(data, [now, data]);
+
+            drawLineChart('#graph-cpu', cpuSeries, 'red', [[now - 100000, now], [100, 0]]);
+        },
+        ip: function(data) {
+            $('#status-ip').html(data);
+        },
+        network: function(data) {
+            $('#status-network-recv').html(formatBytes(data.recv, 2));
+            $('#status-network-sent').html(formatBytes(data.sent, 2));
+        },
+        services: function(data) {
+            $('#services').empty();
+
+            $.each(data, function(i, v) {
+                if (v.status == '+') {
+                    var icon = 'ok'
+                } else if(v.status == '-') {
+                    var icon = 'remove';
+                } else {
+                    var icon = 'question';
+                }
+
+                $('<div>')
+                    .addClass('row')
+                    .html(v.name)
+                    .appendTo('#services');
+            });
+        }
+    };
+
+    var ws = new WebSocket(addr);
+
+    ws.onopen = function() {
+        $('#status').addClass('online').html('Online');
+    };
+
+    ws.onmessage = function(msg) {
+        $.each(JSON.parse(msg.data), function(key, data) {
+            if (key in callbacks) {
+                callbacks[key](data);
+            }
+        });
+    };
+
+    ws.onclose = function() {
+        $('[id^=status-]').html('-');
+        $('#services').html('Offline');
+        $('#status').removeClass('online')
+                    .addClass('offline')
+                    .html('Offline');
+
+        window.setTimeout(function() {
+            connect(addr);
+        }, 10000);
+    }
+};
+
